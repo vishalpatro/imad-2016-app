d1a183d79f60382ab6b4ba968796964a31446603
vishal <vishalpatro123@gmail.com>
2016-11-07 22:43:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
+//we can only send a string as a response
+
 app.get('/:articleName', function(req, res) {
     //articleName = article-one
     //articles[articleName] = {} content for article one
