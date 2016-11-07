991f192b0e1b02ef1cf604092e12ff5854cd54c4
vishal <vishalpatro123@gmail.com>
2016-11-07 18:29:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //move the image
-var image = document.getElementById('vishal');
+var img = document.getElementById('vishal');
 var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
-    vishal.style.marginLeft = marginLeft + 'px';//concatenating 10 with px
+    img.style.marginLeft = marginLeft + 'px';//concatenating 10 with px
 }
-vishal.onclick = function(){
+img.onclick = function(){
     var interval = setInterval(moveRight,100);
 };
