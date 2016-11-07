baba5afaae6efff50363ac330079037d525ebffa
vishal <vishalpatro123@gmail.com>
2016-11-07 18:20:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New value';
+
+//move the image
+var image = document.getElementById('vishal');
+vishal.onclick = function(){
+    img.style.marginLeft = '100px';
+};
