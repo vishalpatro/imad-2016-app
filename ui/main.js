d221647bc5a3df4499089ae2c177f6f7270aa406
vishal <vishalpatro123@gmail.com>
2016-11-08 19:10:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,7 @@ button.onclick = function() {
             if(request.readyState === XMLHttpRequest.DONE) {
                 //TAKE some action
                 if(request.status === 200){
+                    //code to increament counter
                     var counter = request.responseText;
                     var span = document.getElementById('count');
                     span.innerHTML = counter.toString();
@@ -30,21 +31,37 @@ button.onclick = function() {
     span.innerHTML = counter.toString();*/
 };
 
-//submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-  //make a req to a server and send the name
   
-  
-  
-  //capture a list of names and render it as a list
-  var names = ['name1','name2','name3','name4'];
-  var list = '';
-  for(var i=0; i<names.length; i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+  //create a request object
+        var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a var
+        request.onreadystatechange = function(){
+            if(request.readyState === XMLHttpRequest.DONE) {
+                //TAKE some action
+                if(request.status === 200){//condition means that request is successfull or not
+                    //capture a list of names and render it as a list
+                      //var names = ['name1','name2','name3','name4'];
+                      var names = request.responseText;
+                      names = JSON.parse(names);//converting string to object,in this case,an array
+                      var list = '';
+                      for(var i=0; i<names.length; i++){
+                          list += '<li>' + names[i] + '</li>';
+                      }
+                      var ul = document.getElementById('namelist');
+                      ul.innerHTML = list;//rendering list of names
+                }
+            }
+        };
+        
+        
+        //submit name or extract of name after clicking of button
+        var nameInput = document.getElementById('name');
+        var name = nameInput.value;//inputbox value
+        //make a request
+        request.open('GET','http://vishalpatro.imad.hasura-app.io/submit-name?name=' + name, true);//value of name comes from inputbox
+        request.send(null);
 };
