02f70b9d03568119ab42331bd5ee3dcf509149ef
vishal <vishalpatro123@gmail.com>
2016-11-22 16:30:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,15 +148,25 @@ app.get('/submit-name', function (req, res) {
 
 
 
-app.get('/:articleName', function(req, res) {
+app.get('/articles/:articleName', function(req, res) {
     //articleName = article-one
     //articles[articleName] = {} content for article one
-    var articleName = req.params.articleName;
-    //this abv statement is use to extract the article name  and use into the index of the articles object whch is used to create template
-    res.send(createTemplate(articles[articleName])); 
+     pool.query("select * from article where title = '" + req.params.articleName + "'", function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        }else {
+            if(result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            }
+            else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
-app.get('/articles/:articleName', function(req, res) {
+/*app.get('/articles/:articleName', function(req, res) {
     //articleName = article-one
     //articles[articleName] = {} content for article one
     //var articleName = req.params.articleName;
@@ -175,7 +185,7 @@ app.get('/articles/:articleName', function(req, res) {
             }
         }
     });
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
