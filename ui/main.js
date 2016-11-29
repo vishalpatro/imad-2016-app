66d97a75a2b178e7ba69e9aa3237b9d88e84c6af
vishal <vishalpatro123@gmail.com>
2016-11-29 10:11:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -73,9 +73,9 @@ submit.onclick = function(){
 
 function loadLoginForm () {
     var loginHtml = `
-        <h3>Login/Register to unlock awesome features</h3>
+        
         <input type="text" id="username" placeholder="username" />
-        <input type="password" id="password" />
+        <input type="password" id="password" placeholder="password" />
         <br/><br/>
         <input type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
